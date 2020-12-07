13a3c6b8bf47b97c48c8c9f10a3f39b094ae39db
LuyingP <luyingyuan@hotmail.com>
2020-12-07 16:43:35 +0100

    delete ontime

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors")
+const schedule = require('node-schedule');
+const fs = require('fs')
 const fileController = require("./controllers/uploadController")
 const ejs = require('ejs');
 app.set('view engine', 'ejs');
@@ -17,7 +19,26 @@ app.use(express.static('assets/images'));
 app.post("/upload", fileController.uploadUnImage);
 app.get("/upload/:fileName", fileController.getFile)
 
+// const rule = new schedule.RecurrenceRule();
+// rule.hour = 16;
+// rule.minute = 36;
+var deleteFile = schedule.scheduleJob('0 0 * * *', function () {
+
+    const path = './assets/images'
+
+    try {
+        fs.rmdir(path, { recursive: true },()=>{
+            console.log("delete ok")
+        })
+    } catch (err) {
+        console.log(err);
+    }
+
+});
+
+
 app.listen(3030, () => {
+    deleteFile;
     console.log("server is running at 3030");
 })
 
